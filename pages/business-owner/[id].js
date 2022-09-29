5b7d63a956e674e86584ff92dbc2137c8cf96d70
samurai1979 <xaiyax01@gmail.com>
2022-09-29 12:16:41 +0100

    added business owner profile page

--- pages/business-owner/[id].js
+++ pages/business-owner/[id].js
@@ -1,12 +1,172 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React from 'react'
 import LandingLayout from '../../layouts/Landing.layout'
+import { BackImage, Bottom, Controls, CreatorDetails, CreatorsCard, CreatorsWrapper, HeroSectionOne, ProfileDetails, ProfileImgCont, SocialCard, SocialHandle, SocialWrapper, Top, TopImg, UserCard, Wrapper } from '../../styles/business-owner'
 
 const BusinessProfile = () => {
    const router = useRouter()
    const {id} = router.query
   return (
-    <div>{id}</div>
+    <div>
+      <HeroSectionOne>
+        <BackImage>
+          <Image src="/profile.png" alt="" layout="fill" objectPosition="center" objectFit="cover"/>
+          <UserCard>
+            <ProfileImgCont>
+              <Image src="/profile-2.png" alt="" layout='fill' objectPosition="center" objectFit="cover"/>
+            </ProfileImgCont>
+            <ProfileDetails>
+              <h2>Business Name</h2>
+              <p><span>Category:</span> fashion &amp; lifestyle</p>
+              <p><span>Location:</span> Lagos, Nigeria</p>
+              <p><span>Website:</span> https://influenzit.com</p>
+              <p>
+              Excepteur sint occaecat cupidatat non proident, saeunt in culpa qui officia deserunt mollit anim laborum. Seden utem perspiciatis undesieu omnis voluptatem accusantium doque laudantium, totam rem aiam eaqueiu ipsa quae ab illoion inventore veritatisetm quasitea architecto beataea dictaed quia couuntur magni dolores eos aquist ratione vtatem seque nesnt.
+              </p>
+            </ProfileDetails>
+          </UserCard>
+        </BackImage>
+        <Wrapper>
+          <h3>Social Profiles</h3>
+          <SocialWrapper>
+            <SocialCard>
+              <Top>
+                <Image src="/instagram.svg" alt="" height={25} width={25}/>
+                <p>Instagram</p>
+              </Top>
+              <Bottom>
+                <Link href="/" targer="_blank" passHref>
+                  <a>itzphoenixgold (774 followers)</a>
+                </Link>
+              </Bottom>
+            </SocialCard>
+            <SocialCard>
+              <Top>
+                <Image src="/twitter.svg" alt="" height={25} width={25}/>
+                <p>Twitter</p>
+              </Top>
+              <Bottom>
+                <Link href="/" targer="_blank" passHref>
+                  <a>itzphoenixgold (774 followers)</a>
+                </Link>
+              </Bottom>
+            </SocialCard>
+            <SocialCard>
+              <Top>
+                <Image src="/facebook.svg" alt="" height={25} width={25}/>
+                <p>Facebook</p>
+              </Top>
+              <Bottom>
+                <Link href="/" targer="_blank" passHref>
+                  <a>itzphoenixgold (774 followers)</a>
+                </Link>
+              </Bottom>
+            </SocialCard>
+            <SocialCard>
+              <Top>
+                <Image src="/tiktok.svg" alt="" height={25} width={25}/>
+                <p>TikTok</p>
+              </Top>
+              <Bottom>
+                <Link href="/" targer="_blank" passHref>
+                  <a>itzphoenixgold (774 followers)</a>
+                </Link>
+              </Bottom>
+            </SocialCard>
+          </SocialWrapper>
+          <h3>Creators we&apos;ve worked with</h3>
+          <CreatorsWrapper>
+            <CreatorsCard>
+              <TopImg>
+                <Image src="/profile-2.png" alt="" layout="fill" objectPosition="center"/>
+              </TopImg>
+              <CreatorDetails>
+                <h4>Ezekiel Phoenixgold</h4>
+                <p>Male | Lagos, Nigeria</p>
+                <SocialHandle>
+                  <Link href="/" targer="_blank" passHref>
+                    <a><Image src="/twitter.svg" alt="" height={30} width={30}/><span>itzphoenixgold | 16k reach</span></a>
+                  </Link>
+                </SocialHandle>
+                <p>Laptop Lifestyle | Photography | Fashion</p>
+                <Controls>
+                  <Link href="/" passHref>
+                    <a>Engage</a>
+                  </Link>
+                  <button><Image src="/list.svg" alt="" height={24} width={24}/></button>
+                </Controls>
+              </CreatorDetails>
+            </CreatorsCard>
+            <CreatorsCard>
+              <TopImg>
+                <Image src="/profile-2.png" alt="" layout="fill" objectPosition="center"/>
+              </TopImg>
+              <CreatorDetails>
+                <h4>Ezekiel Phoenixgold</h4>
+                <p>Male | Lagos, Nigeria</p>
+                <SocialHandle>
+                  <Link href="/" targer="_blank" passHref>
+                    <a><Image src="/twitter.svg" alt="" height={30} width={30}/><span>itzphoenixgold | 16k reach</span></a>
+                  </Link>
+                </SocialHandle>
+                <p>Laptop Lifestyle | Photography | Fashion</p>
+                <Controls>
+                  <Link href="/" passHref>
+                    <a>Engage</a>
+                  </Link>
+                  <button><Image src="/list.svg" alt="" height={24} width={24}/></button>
+                </Controls>
+              </CreatorDetails>
+            </CreatorsCard>
+            <CreatorsCard>
+              <TopImg>
+                <Image src="/profile-2.png" alt="" layout="fill" objectPosition="center"/>
+              </TopImg>
+              <CreatorDetails>
+                <h4>Ezekiel Phoenixgold</h4>
+                <p>Male | Lagos, Nigeria</p>
+                <SocialHandle>
+                  <Link href="/" targer="_blank" passHref>
+                    <a><Image src="/twitter.svg" alt="" height={30} width={30}/><span>itzphoenixgold | 16k reach</span></a>
+                  </Link>
+                </SocialHandle>
+                <p>Laptop Lifestyle | Photography | Fashion</p>
+                <Controls>
+                  <Link href="/" passHref>
+                    <a>Engage</a>
+                  </Link>
+                  <button><Image src="/list.svg" alt="" height={24} width={24}/></button>
+                </Controls>
+              </CreatorDetails>
+            </CreatorsCard>
+            <CreatorsCard>
+              <TopImg>
+                <Image src="/profile-2.png" alt="" layout="fill" objectPosition="center"/>
+              </TopImg>
+              <CreatorDetails>
+                <h4>Ezekiel Phoenixgold</h4>
+                <p>Male | Lagos, Nigeria</p>
+                <SocialHandle>
+                  <Link href="/" targer="_blank" passHref>
+                    <a><Image src="/twitter.svg" alt="" height={30} width={30}/><span>itzphoenixgold | 16k reach</span></a>
+                  </Link>
+                </SocialHandle>
+                <p>Laptop Lifestyle | Photography | Fashion</p>
+                <Controls>
+                  <Link href="/" passHref>
+                    <a>Engage</a>
+                  </Link>
+                  <button><Image src="/list.svg" alt="" height={24} width={24}/></button>
+                </Controls>
+              </CreatorDetails>
+            </CreatorsCard>
+          </CreatorsWrapper>
+        </Wrapper>
+      </HeroSectionOne>
+      
+    </div>
   )
 }
 BusinessProfile.getLayout = (page) => (
---
+++ styles/business-owner.js
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+import { colors, sizes } from "./theme";
+
+export const HeroSectionOne = styled.section`
+`;
+export const BackImage = styled.div`
+    position: relative;
+    height: 60vh;
+    min-height: 400px;
+`;
+export const UserCard = styled.div`
+    background: #fff;
+    position: absolute;
+    z-index: 99;
+    width: 99vw;
+    max-width: calc(((100vw - ${sizes.wrapperWidth})/2) + ${sizes.wrapperWidth});
+    padding: 20px 20px 20px calc((100vw - ${sizes.wrapperWidth})/2);
+    bottom: -100px;
+    border-radius: 3px;
+    display: flex;
+    P {
+        color: #666666;
+        span {
+            font-weight: 600;
+            color: #111;
+        }
+    }
+    p:last-child {
+        color: #333;
+        margin-top: 10px;
+    }
+`;
+export const ProfileImgCont = styled.div`
+    height: 250px;
+    position: relative;
+    width: 250px;
+    min-width: 250px;
+    margin-right: 20px;
+`;
+export const ProfileDetails = styled.div`
+`;
+export const Wrapper = styled.div`
+    padding-top: 130px;
+    width: 98%;
+    max-width: ${sizes.wrapperWidth};
+    margin: 0 auto;
+    h3 {
+        color: #333;
+        padding: 10px 0;
+        border-bottom: 1px solid #333;
+    }
+`;
+export const SocialWrapper = styled.div`
+    padding: 20px 0;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+`;
+export const SocialCard = styled.div`
+    border: 1px solid #d2d2d2;
+    width: 48%;
+    max-width: 48%;
+    min-width: 320px;
+    border-radius: 3px;
+    margin: 10px 0;
+`;
+export const Top = styled.div`
+    padding: 10px;
+    display: flex;
+    border-bottom: 1px solid #d2d2d2;
+    p {
+        padding-left: 10px;
+    }
+`;
+export const Bottom = styled.div`
+    padding: 10px;
+`;
+export const CreatorsWrapper = styled.div`
+    padding: 20px 0;
+    display: flex;
+    column-gap: 15px;
+    flex-wrap: wrap;
+    justify-content: space-between;
+`;
+export const CreatorsCard = styled.div`
+    height: 370px;
+    width: 290px;
+    border: 1px solid #D2D2D2;
+    text-align: center;
+    p {
+        font-size: 14px;
+        color: #999;
+    }
+`;
+export const TopImg = styled.div`
+    height: 50%;
+    width: 100%;
+    position: relative;
+`;
+export const CreatorDetails = styled.div`
+    padding: 5px;
+`;
+export const SocialHandle = styled.div`
+    font-size: 14px;
+    padding: 5px 0;
+    span {
+        padding-left: 10px;
+    }
+    a {
+        display: inline-flex;
+        align-items: center;
+    }
+`;
+export const Controls = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 10px 0;
+    a {
+        background: ${colors.primaryColor};
+        color: #fff;
+        transition: .5s ease;
+        border: 2px solid ${colors.primaryColor};
+        padding: 5px 15px;
+        margin-right: 10px;
+        :hover {
+            background: white;
+            color: ${colors.primaryColor};
+        }
+    }
+    button {
+        background: transparent;
+        border: none;
+    }
+`;
