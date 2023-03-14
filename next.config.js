50186430576dfe9800d1a834cdf70b8d445b5d42
samurai1979 <xaiyax01@gmail.com>
2023-03-14 12:30:50 +0100

    redirect

--- next.config.js
+++ next.config.js
@@ -23,23 +23,7 @@ const nextConfig = {
       },
     ],
   },
-
   trailingSlash: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path((?!_next|influencer-waitlist|business-waitlist$).*)/",
-        has: [
-          {
-            type: 'header',
-            key: 'x-redirect-me'
-          }
-        ],
-        destination: "/influencer-waitlist",
-        permanent: true
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
