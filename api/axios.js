0894f5aed3e1ced1f9c773a220a3f70a593b69f5
Laolu <bolagbemi@gmail.com>
2024-09-16 18:40:54 +0100

    Update axios.js

--- api/axios.js
+++ api/axios.js
@@ -5,8 +5,7 @@ export const axiosInstance = () => {
     // fetches token from local storage 
     const token = typeof window !== "undefined" && localStorage.getItem("token");
     return axios.create({
-        // baseURL: `${process.env.NEXT_PUBLIC_API_URI}/api/v1`,
-        baseURL: `https://influenzit.com/api/v1`,
+        baseURL: `${process.env.NEXT_PUBLIC_API_URI}/api/v1`,
         headers: {
             Authorization: !!token ? `Bearer ${token}` : "",
             
